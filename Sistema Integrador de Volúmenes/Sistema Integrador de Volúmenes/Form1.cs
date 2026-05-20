17c5ee82ebba3c812d0673177e7500316f039dec
Caleb-Ochoa <cochoar@unicesar.edu.co>
2026-05-20 13:13:50 -0500

    Proyecto

---
+++ Sistema Integrador de Volúmenes/Sistema Integrador de Volúmenes/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_Integrador_de_Volúmenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
